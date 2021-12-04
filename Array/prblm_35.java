a4638868d2dc7b9703b50b188212c4ebcdcf5b24
Ayush Gangwar <ayush.gangwar210899@gmail.com>
2021-12-04 17:44:10 +0530

    prblm_35

---
+++ Array/prblm_35.java
@@ -0,0 +1,13 @@
+class prblm_35 {
+    public int searchInsert(int[] nums, int target) {
+for(int i = 0; i < nums.length; i++){
+if(nums[i] >= target) return i;
+}
+// the target should be inserted at the end of the array
+return nums.length;
+}
+}
+        
+        
+    
+
