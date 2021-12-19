1e0af7abad3f45839a7dc47182a29d387a261c55
Ayush Gangwar <ayush.gangwar210899@gmail.com>
2021-12-19 21:44:22 +0530

    prblm 2108

---
+++ prblm_2108.java
@@ -0,0 +1,15 @@
+class prblm_2108 {
+    public String firstPalindrome(String[] words) {
+        int i,j;
+        for(String s:words)
+        {
+            i=-1;
+            j=s.length();
+            while(++i<--j&&s.charAt(i)==s.charAt(j));
+            if(i>=j)
+                return s;
+        }
+        return "";
+        
+    }
+}
