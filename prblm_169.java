15b3a57225a5703d4aa0eaf9710c14ca9255f7ed
Ayush Gangwar <ayush.gangwar210899@gmail.com>
2021-12-25 15:41:27 +0530

    majority number return

---
+++ prblm_169.java
@@ -0,0 +1,14 @@
+class prblm_169 {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+if(nums[0] == nums[nums.length/2]){
+    return nums[nums.length/2];
+}
+else if(nums[nums.length-1]==nums[nums.length/2]) {
+return nums[nums.length/2];
+}
+     return nums[nums.length/2];   
+}
+}
+        
+    
