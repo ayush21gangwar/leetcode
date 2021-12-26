1818226929870df8992683c73f04afbeea0b58d5
Ayush Gangwar <ayush.gangwar210899@gmail.com>
2021-12-27 00:01:53 +0530

    contains duplicate

---
+++ prblm_217.java
@@ -0,0 +1,10 @@
+class prblm_217 {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==nums[i+1]){
+                return true;
+            }
+        }
+        return false;
+}  }
