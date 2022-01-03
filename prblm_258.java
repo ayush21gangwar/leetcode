608c4d740c5dfbdd1ce721a22f069aad15587ead
Ayush Gangwar <ayush.gangwar210899@gmail.com>
2022-01-03 21:37:19 +0530

    add digit

---
+++ prblm_258.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int addDigits(int num) {
+     
+        while(num>=10){
+            int sum=0;
+            for( ;num>0;sum+=num%10,num=num/10);
+            num=sum;
+        }
+        return num;       
+    }
+}
