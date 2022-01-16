05ce8739f20d9f78a95f0aea54daae7c2b87f3e8
Ayush Gangwar <ayush.gangwar210899@gmail.com>
2022-01-16 22:02:13 +0530

    version control

---
+++ prblm_278.java
@@ -0,0 +1,20 @@
+public class prblm_278 extends VersionControl {
+    public int firstBadVersion(int n) {
+          int first=1,last=n,ans=-1;
+            while(first<=last)
+            {
+                int mid=first+(last-first)/2;
+                if(isBadVersion(mid))
+                   {
+                       ans=mid;
+                       last=mid-1;
+                   }
+                   else
+                   {
+                       first=mid+1;
+                   }
+            }
+            return ans;
+        
+    }
+}
