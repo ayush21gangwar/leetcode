1362d387040b63fd881aee017b1d198024805b97
Ayush Gangwar <ayush.gangwar210899@gmail.com>
2022-01-21 17:04:40 +0530

    self dividing

---
+++ prblm_728.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> ans = new ArrayList<Integer>();
+        boolean add;
+        int n,d;
+        for(int i=left;i<=right;i++) {
+            n = i;
+            add = true;
+            while(n > 0) {
+                d = n%10;
+                if(d == 0 || i % d != 0){
+                    add = false;
+                    break;
+                }
+                n/=10;
+            }
+            if(add) ans.add(i);
+        }
+        return ans;
+    }
+}
