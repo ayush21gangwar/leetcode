ce65f4e1d4ea008d64241a64fcc6f17684e87778
Ayush Gangwar <ayush.gangwar210899@gmail.com>
2022-01-23 21:18:20 +0530

    happy number

---
+++ prblm_202.java
@@ -0,0 +1,15 @@
+class prblm_202 {
+    public:
+        bool isHappy(int n) {
+            int num = n , digit , sum=0;
+            if(n>1 && n<7) return false;
+            while(n>0)
+            {
+                digit=n%10;
+                sum += digit*digit;
+                n=n/10;
+            }
+            if(sum==1)return true;
+            else return isHappy(sum);
+        }
+    };
