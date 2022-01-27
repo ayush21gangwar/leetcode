acf809c6842e082614a7f3bcc1d6d1b83937c916
Ayush Gangwar <ayush.gangwar210899@gmail.com>
2022-01-27 22:53:17 +0530

    jump game

---
+++ prblm_55.java
@@ -0,0 +1,13 @@
+class prblm_55 {
+    public boolean canJump(int[] nums) {
+          int far = 0;
+        for(int i=0;i<nums.length;i++){
+            if(far<i){
+                return false;
+            }
+            far=Math.max(far,nums[i]+i);
+        }
+        return true;
+        
+    }
+}
