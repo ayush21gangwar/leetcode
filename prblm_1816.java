b269bc92dc8804a15cf7cec26d0b61330c0aaf85
Ayush Gangwar <ayush.gangwar210899@gmail.com>
2022-02-12 19:37:42 +0530

    truncate sentence

---
+++ prblm_1816.java
@@ -0,0 +1,15 @@
+class prblm_1816 {
+    public String truncateSentence(String s, int k) {
+           int space = 0;
+        for(int i = 0 ; i< s.length(); i++){
+            if(s.charAt(i) == ' '){
+             space +=1;  
+             if(space == k){ 
+             return s.substring(0, i);  
+                }
+            }
+        }
+        return s;
+        
+    }
+}
