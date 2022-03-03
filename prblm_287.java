479a1a9d4de0d32fccfd721a344fe94cc1ab9e24
Ayush Gangwar <ayush.gangwar210899@gmail.com>
2022-03-03 19:52:56 +0530

    find duplicate

---
+++ prblm_287.java
@@ -0,0 +1,13 @@
+class prblm_287 {
+    public int findDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]){
+                return nums[i];
+            }
+        }
+        return -1;
+        
+    }
+}
