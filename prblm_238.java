de67776259ca154f7793343d37b30df36d123218
Ayush Gangwar <ayush.gangwar210899@gmail.com>
2022-03-04 18:59:58 +0530

    productExceptSelf

---
+++ prblm_238.java
@@ -0,0 +1,14 @@
+class prblm_238 {                        
+    public int[] productExceptSelf(int[] nums) {
+        int[] output = new int[nums.length];
+		for (int i = 0, left = 1; i < nums.length; i++) {
+			output[i] = left;
+			left *= nums[i];
+		}
+		for (int i = nums.length - 1, right = 1; i >= 0; i--) {
+			output[i] *= right;
+			right *= nums[i];
+		}
+		return output;
+    }
+}
