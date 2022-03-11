f12c45dccc909780eab881b5e58ac5e38ee391ff
Ayush Gangwar <ayush.gangwar210899@gmail.com>
2022-03-11 21:03:02 +0530

    numbers

---
+++ Array/prblm_1952.java
@@ -0,0 +1,19 @@
+public class prblm_1952 {
+    public boolean isThree(int n) {
+         int count=0;
+       for(int i=1;i<=n;i++)
+       {
+           if(n%i==0)
+           {
+               count++;
+           }
+       }
+        if(count==3)
+        {
+            return true;
+        }
+        return false;
+    }
+}class prblm_1952 {
+    
+}
