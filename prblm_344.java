f0f45f5388cb7f40add714e43aacaef8ca2755c3
Ayush Gangwar <ayush.gangwar210899@gmail.com>
2022-03-24 23:07:21 +0530

    reverse string

---
+++ prblm_344.java
@@ -0,0 +1,16 @@
+class prblm_344 {
+    public void reverseString(char[] s) {
+         int i=0;
+        int j=s.length-1;
+        char temp;
+        while(i<j)
+        {
+            temp=s[i];
+            s[i]=s[j];
+            s[j]=temp;
+            i++;
+            j--;
+        
+    }
+}
+}
