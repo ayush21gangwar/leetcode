b50ba0271b50629d01566c5a91c2fbc1487a78e4
Ayush Gangwar <ayush.gangwar210899@gmail.com>
2022-03-30 22:28:17 +0530

    Sorted Squares

---
+++ prblm_977.java
@@ -0,0 +1,10 @@
+class prblm_977 {
+    public int[] sortedSquares(int[] nums) {
+         for(int i = 0; i<nums.length;i++){
+         nums[i] = nums[i]*nums[i]; 
+        }
+        Arrays.sort(nums);
+        return nums;
+        
+    }
+}
