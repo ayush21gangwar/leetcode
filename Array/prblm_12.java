60a7a8d0668552f3789e4ba7ce19bf4f4a389a41
Ayush Gangwar <ayush.gangwar210899@gmail.com>
2022-04-29 22:48:12 +0530

    int to roman

--- Array/prblm_12.java
+++ Array/prblm_12.java
@@ -2,8 +2,7 @@ class prblm_12 {
     public String intToRoman(int num) {
         int[] a = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
     String[] romans = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-	
-    public String intToRoman(int num) {
+
         StringBuilder roman = new StringBuilder();
         int idx = 0;
         while(num > 0) {
