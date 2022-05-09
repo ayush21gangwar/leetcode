d76dc0d34651923cae1e058dca4b2259b978317d
Ayush Gangwar <ayush.gangwar210899@gmail.com>
2022-05-09 20:12:14 +0530

    largest perimeter

---
+++ Array/prblm_976.java
@@ -0,0 +1,16 @@
+class prblm_976 {
+    public int largestPerimeter(int[] nums) {
+        Arrays.sort(nums);
+        int perimeter=0;
+        for(int i=nums.length-1;i>=2;i--)
+        {
+            if(nums[i-1]+nums[i-2]>nums[i])
+            {
+                perimeter=nums[i]+nums[i-1]+nums[i-2];
+                break;
+            }
+        }
+        return perimeter;
+        
+    }
+}
