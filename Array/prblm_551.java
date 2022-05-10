adc88a25608e2912ecd8a6d6da52598291ba1930
Ayush Gangwar <ayush.gangwar210899@gmail.com>
2022-05-10 20:08:36 +0530

    check record

---
+++ Array/prblm_551.java
@@ -0,0 +1,12 @@
+ class prblm_551 {
+    public boolean checkRecord(String s) {
+          if(s.indexOf("A") != s.lastIndexOf("A") || s.contains("LLL"))
+    {
+            return false;
+    }
+        return true;
+        
+    }
+} 
+    
+
