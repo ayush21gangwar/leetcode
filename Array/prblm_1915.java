b0a445aa09bbab35ac77ae28e6febbdc92dbec46
Ayush Gangwar <ayush.gangwar210899@gmail.com>
2022-05-18 21:23:18 +0530

    WONDERFUL SUBSTRING

---
+++ Array/prblm_1915.java
@@ -0,0 +1,16 @@
+class prblm_1915 {
+    public long wonderfulSubstrings(String word) {
+         long cnt[] = new long[1024], res = 0;
+    int mask = 0;
+    cnt[0] = 1;
+    for (var ch : word.toCharArray()) {
+        mask ^= 1 << (ch - 'a');
+        res += cnt[mask];
+        for (var n = 0; n < 10; ++n)
+            res += cnt[mask ^ (1 << n)];
+        ++cnt[mask];
+    }
+    return res;
+        
+    }
+}
