467c3ab54ac81c5e27bac308e9234cd4eb7ec3d8
Ayush Gangwar <ayush.gangwar210899@gmail.com>
2022-05-24 20:51:20 +0530

    Square sum

---
+++ Array/prblm_633.java
@@ -0,0 +1,26 @@
+class prblm_633 {
+    public boolean judgeSquareSum(int c) {
+        int left=0,right=(int)Math.sqrt(c);
+        while(left<=right)
+        {
+           int s=(left*left)+(right*right);
+            if(s<0)
+            {
+                right--;
+            }
+            if(s==c)
+            {
+                return true;
+            }
+             if(s<c)
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+        return false;
+    }
+}
