2c7359dbcb97eff457e3671b47d2ae4afc39831e
Ayush Gangwar <ayush.gangwar210899@gmail.com>
2022-06-05 17:09:40 +0530

    triples

---
+++ Array/prblm_1925.java
@@ -0,0 +1,20 @@
+class prblm_1925 {
+    public int countTriples(int n) {
+        int count=0;
+     for(int a=1;a<=n;a++)
+     {
+         for(int b=1;b<=n;b++)
+         {
+             int temp=Math.max(a,b);
+             for(int c=temp;c<=n;c++)
+             {
+                 if(a*a+b*b==c*c)
+                 {
+                     count++;
+                 }
+             }
+         }
+     }
+        return count;
+    }
+}
