38040589b2ef4ddb609eb01c5b81393d3525cca2
Ayush Gangwar <ayush.gangwar210899@gmail.com>
2022-07-15 12:32:42 +0530

    removeNthFromEnd

---
+++ Array/prblm_19.java
@@ -0,0 +1,27 @@
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if(head.next==null){
+            return null;
+        }
+        int size=0;
+        ListNode curr= head;
+        while(curr!=null){
+            curr= curr.next;
+            size++;
+        }
+        if(n==size){
+            return head.next;
+        }
+        int indextoSearch=size-n;
+        ListNode prev= head;
+        int i=1;
+        while(i<indextoSearch){
+            prev=prev.next;
+            i++;
+        }
+        prev.next=prev.next.next;
+        return head;
+        
+        
+    }
+}
